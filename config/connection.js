2bdc0c1d638bdfd3d814ed4374e50df3567ee3d7
Brandon James <bjames9076@gmail.com>
2018-01-25 17:24:24 -0500

    added mongodb

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+
+
+
+
+module.exports = {
+    
+        'url' : "mongodb://kay:myRealPassword@mycluster0-shard-00-00.mongodb.net:27017,mycluster0-shard-00-01.mongodb.net:27017,mycluster0-shard-00-02.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin" // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    
+    };
