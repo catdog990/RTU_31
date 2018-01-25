181b94f37ca849ed8aeec3a4f8ad303f183c519f
Brandon James <bjames9076@gmail.com>
2018-01-25 16:36:05 -0500

    config server

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+// server.js
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+// set up ======================================================================
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
 
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
+var configDB = require('./config/database.js');
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+// require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'stuff' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./routes/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('Listening on port: ' + port);
