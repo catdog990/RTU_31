3016958610c7aa8d195f3697013031e869f4d926
Brandon James <bjames9076@gmail.com>
2018-01-25 19:25:11 -0500

    connected to the mongo server!

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,13 @@
 
 module.exports = {
     
-        'url' : "mongodb://kay:myRealPassword@mycluster0-shard-00-00.mongodb.net:27017,mycluster0-shard-00-01.mongodb.net:27017,mycluster0-shard-00-02.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin" // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    "url" : "mongodb://rtu:root@rtuproject-shard-00-00-salqe.mongodb.net:27017,rtuproject-shard-00-01-salqe.mongodb.net:27017,rtuproject-shard-00-02-salqe.mongodb.net:27017/users?ssl=true&replicaSet=rtuProject-shard-0&authSource=admin" // connect to our database
+    // required for passport
+    
+
     
     };
+
+    
+
+
