0a54a501ac2b7fc3d6f582e288190fab05fc251d
Brandon <bjames9076@gmail.com>
2018-01-29 11:50:40 -0500

    dynamic username

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@
 var db = require("../modelsSQL");
 
 
+    
 
 module.exports = function(app, passport) {
 
@@ -58,7 +59,7 @@ module.exports = function(app, passport) {
 
     // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/profile', // redirect to the secure profile section
+        successRedirect : '/account', // redirect to the secure profile section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
@@ -69,9 +70,14 @@ module.exports = function(app, passport) {
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('profile.ejs', {
-            user : req.user // get the user out of session and pass to template
-        });
+        db.User.findOne({
+            where: {
+              User: req.user.local.email
+            }
+          }).then(function(dbUser){
+            res.render('profile.ejs', { stuff: dbUser.dataValues });
+            });
+        
     });
 
     // =====================================
