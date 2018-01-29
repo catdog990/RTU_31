447c5124c35ceebcf60b9e9222306d027b2923a5
Brandon James <bjames9076@gmail.com>
2018-01-29 00:36:20 -0500

    added sequelize

--- models/rtu.js
+++ models/rtu.js
@@ -1,8 +1,9 @@
 
 //schema for user table
 module.exports = function(sequelize, Datatypes){
-    var user = sequelize.define("User",{
+    var User = sequelize.define("User",{
         User: Datatypes.STRING,
         Job: Datatypes.STRING,
-    })
-}
+    });
+    return User
+};
