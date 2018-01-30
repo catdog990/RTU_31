5397bbd8729f3244e11c0327b2fdd4aa29c1e3e1
Brandon James <bjames9076@gmail.com>
2018-01-29 22:10:14 -0500

    auto save email with name and job to db

--- modelsSQL/rtu.js
+++ modelsSQL/rtu.js
@@ -2,8 +2,9 @@
 //schema for user table
 module.exports = function(sequelize, Datatypes){
     var User = sequelize.define("User",{
+        Email: Datatypes.STRING,
         User: Datatypes.STRING,
-        Job: Datatypes.STRING,
+        Job: Datatypes.STRING
     });
     return User;
 };
